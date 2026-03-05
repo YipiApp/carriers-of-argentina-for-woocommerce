0b24f06aa8d37ee6f9423084ac1103f6d86fa4a3
Kijam López <kijamve@gmail.com>
2026-03-05 10:08:23 -0400

    Update version to 1.5.7 in class-wc-kshippingargentina.php

--- class-wc-kshippingargentina.php
+++ class-wc-kshippingargentina.php
@@ -12,7 +12,7 @@
  * Description: Carriers of Argentina for WooCommerce
  * Author: Kijam López
  * Author URI: https://github.com/kijamve/carriers-of-argentina-for-woocommerce
- * Version: 1.5.6
+ * Version: 1.5.7
  * License: GPLv2
  * Text Domain: carriers-of-argentina-for-woocommerce
  * Domain Path: /languages/
@@ -34,7 +34,7 @@ if ( ! class_exists( 'WC_KShippingArgentina' ) ) :
 		 * @var string
 		 */
 
-		const VERSION = '1.5.6';
+		const VERSION = '1.5.7';
 
 		/**
 		 * Instance of this class.
